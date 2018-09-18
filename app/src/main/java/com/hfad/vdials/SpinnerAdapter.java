d3d42347cc63dba28de9a90bb30d7823a4d0c615
callummarvell <C.Marvell@warwick.ac.uk>
2018-09-19 00:07:59 +0100

    Tweaked Spinners to look nicer. More readable blues, better sizing/weighting

--- app/src/main/java/com/hfad/vdials/SpinnerAdapter.java
+++ app/src/main/java/com/hfad/vdials/SpinnerAdapter.java
@@ -1,6 +1,7 @@
 package com.hfad.vdials;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,14 +9,33 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+
 public class SpinnerAdapter extends ArrayAdapter<Maneuver>{
     private Context context;
     private Maneuver[] maneuever_array;
+    private int[] blues = new int [14];
 
     public SpinnerAdapter(Context context, int resource, Maneuver[] maneuvers){
         super(context,  R.layout.spinner_value_layout, R.id.spinnerText, maneuvers);
         this.context = context;
         this.maneuever_array = maneuvers;
+        blues[0] = R.drawable.icon_maneuver_bank_left_blue;
+        blues[1] = R.drawable.icon_maneuver_bank_right_blue;
+        blues[2] = R.drawable.icon_maneuver_turn_left_blue;
+        blues[3] = R.drawable.icon_maneuver_turn_right_blue;
+        blues[4] = R.drawable.icon_maneuver_segnor_left_blue;
+        blues[5] = R.drawable.icon_maneuver_segnor_right_blue;
+        blues[6] = R.drawable.icon_maneuver_k_turn_blue;
+        blues[7] = R.drawable.icon_maneuver_stop_blue;
+        blues[8] = R.drawable.icon_maneuver_reverse_bank_left_blue;
+        blues[9] = R.drawable.icon_maneuver_reverse_bank_right_blue;
+        blues[10] = R.drawable.icon_maneuver_reverse_straight_blue;
+        blues[11] = R.drawable.icon_maneuver_tallon_left_blue;
+        blues[12] = R.drawable.icon_maneuver_tallon_right_blue;
+        blues[13] = R.drawable.icon_maneuver_straight_blue;
     }
 
     /*@Override
@@ -54,7 +74,11 @@ public class SpinnerAdapter extends ArrayAdapter<Maneuver>{
         textView.setText(Integer.toString(maneuever_array[position].getSpeed()));
 
         ImageView imageView = (ImageView)row.findViewById(R.id.spinnerImages);
-        imageView.setImageResource(maneuever_array[position].getImageResourceId());
+        int imid = maneuever_array[position].getImageResourceId();
+        imageView.setImageResource(imid);
+
+        if(ArrayUtils.contains(blues,imid))
+            imageView.setColorFilter(ContextCompat.getColor(context, R.color.maneuverBlue), android.graphics.PorterDuff.Mode.MULTIPLY);
 
         return row;
     }
